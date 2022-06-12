9f0f203d880c83b5664d7e612627b65fa2d0aa02
Tristan <tsilvas0113@gmail.com>
2022-06-12 17:03:07 -0500

    Continuing First React project/ setting up locally

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer () {
+    return (
+        <footer className='footer'>
+            <small>© 2022 Silvas development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+export default Footer
