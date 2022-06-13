baf57272641b8d921e3912a31afccba79cd8764a
Tristan <tsilvas0113@gmail.com>
2022-06-13 15:14:16 -0500

    Update main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@ import React from "react"
 
 function Main () {
     return (
-        <h1>Main goes here</h1>
+        <div className="main-container">
+            <h1 className="title">Fun facts about React</h1>
+            <ul className="main-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
 
