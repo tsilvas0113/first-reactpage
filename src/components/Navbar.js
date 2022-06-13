eaf553a0cdfce6785fb2010bf56708e250c1a133
Tristan <tsilvas0113@gmail.com>
2022-06-13 11:06:45 -0500

    Setup project

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import logo from '../react-logo.png'
+
+function Navbar () {
+    return (
+        <h1>Navbar goes here</h1>
+    )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import App from "./App"
 import './index.css'
-
-function App () {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
